1983d42c06d5e31e52ffdd01b68d00e67bbd87d2
orangebaron <orangebaronroblox@gmail.com>
2017-10-09 16:39:04 -0400

    added unit building, unit placing, and unit constructor

--- players.go
+++ players.go
@@ -2,19 +2,24 @@ package main
 
 // player represents a player in the game.
 type player struct {
-	ownedunits  []*unit
-	unitslost   []*unit //units lost since last turn
-	foodO       uint
-	metalO      uint
-	productionO uint
-	managementO uint
-	people      uint
-	peopleR     uint
-	managementR uint
+	ownedunits               []*unit
+	unitslost                []*unit //units lost since last turn
+	foodO                    uint
+	metalO                   uint
+	productionO              uint
+	managementO              uint
+	people                   uint
+	peopleR                  uint
+	managementR              uint
+	buildqueue               []*unittype
+	buildmetalremaining      uint
+	buildproductionremaining uint
+	unitsfinished            []*unittype
 }
 
 // turn is fired every turn.
 func (p *player) turn() {
+	// change resources outputs and required resources
 	for _, u := range p.unitslost {
 		p.foodO -= u.stats.foodO
 		p.metalO -= u.stats.metalO
@@ -23,4 +28,36 @@ func (p *player) turn() {
 		p.peopleR -= u.stats.peopleR
 		p.managementR -= u.stats.managementR
 	}
+	// change what's being built
+	if len(p.buildqueue) > 0 {
+		if p.buildmetalremaining > 0 {
+			p.buildmetalremaining -= p.metalO
+		} else if p.buildproductionremaining > p.productionO {
+			p.buildproductionremaining -= p.productionO
+		} else {
+			// build finished
+			p.buildproductionremaining = 0
+			p.unitsfinished = append(p.unitsfinished, p.buildqueue[0])
+			p.buildqueue = p.buildqueue[1:]
+			if len(p.buildqueue) > 0 {
+				p.buildmetalremaining = p.buildqueue[0].metalC
+				p.buildproductionremaining = p.buildqueue[0].productionC
+			}
+		}
+	}
+}
+
+// placeunit places a unit from a player's finished units list.
+// Returns whether successful or not.
+func (p *player) placeunit(u *unittype, v vec) bool {
+	for i, unit := range p.unitsfinished { // check if user can place that unit
+		if unit == u {
+			if newunit(u, v, p) == nil {
+				return false
+			}
+			p.unitsfinished = append(p.unitsfinished[:i], p.unitsfinished[i+1:]...)
+			return true
+		}
+	}
+	return false
 }
--- units.go
+++ units.go
@@ -1,7 +1,7 @@
 package main
 
-// unitType contains data for a type of unit or tile.
-type unitType struct {
+// unittype contains data for a type of unit or tile.
+type unittype struct {
 	name        string
 	foodO       uint
 	metalO      uint
@@ -18,21 +18,21 @@ type unitType struct {
 }
 
 // types is a list of all possible types.
-var types = [14]unitType{
-	unitType{"Farm", 75, 0, 0, 0, 5, 1, 1, 20, 3, 0, 0, 0},
-	unitType{"Urban Farm", 200, 0, 0, 0, 10, 3, 5, 50, 1, 0, 0, 0},
-	unitType{"Mineshaft", 0, 1, 0, 0, 30, 7, 3, 15, 3, 0, 0, 0},
-	unitType{"Factory", 0, 0, 1, 0, 40, 10, 10, 60, 1, 0, 0, 0},
-	unitType{"Robotic Factory", 0, 0, 3, 0, 0, 7, 20, 120, 1, 0, 0, 0},
-	unitType{"Small Office", 0, 0, 0, 100, 20, 0, 4, 40, 1, 0, 0, 0},
-	unitType{"Office Tower", 0, 0, 0, 300, 50, 0, 10, 100, 1, 0, 0, 0},
-	unitType{"Wall", 0, 0, 0, 0, 0, 0, 10, 15, 10, 0, 0, 0},
-	unitType{"Turret", 0, 0, 0, 0, 2, 1, 15, 30, 5, 0, 1, 5},
-	unitType{"Missile Station", 0, 0, 0, 0, 5, 2, 20, 40, 5, 0, 20, 10},
-	unitType{"Cruiser", 0, 0, 0, 0, 10, 4, 40, 90, 5, 5, 5, 7},
-	unitType{"Frigate", 0, 0, 0, 0, 7, 3, 20, 50, 4, 7, 2, 4},
-	unitType{"Destroyer", 0, 0, 0, 0, 10, 4, 35, 90, 5, 10, 2, 5},
-	unitType{"Aircraft Carrier", 0, 0, 0, 0, 50, 6, 50, 120, 15, 5, 10, 10},
+var types = [14]unittype{
+	unittype{"Farm", 75, 0, 0, 0, 5, 1, 1, 20, 3, 0, 0, 0},
+	unittype{"Urban Farm", 200, 0, 0, 0, 10, 3, 5, 50, 1, 0, 0, 0},
+	unittype{"Mineshaft", 0, 1, 0, 0, 30, 7, 3, 15, 3, 0, 0, 0},
+	unittype{"Factory", 0, 0, 1, 0, 40, 10, 10, 60, 1, 0, 0, 0},
+	unittype{"Robotic Factory", 0, 0, 3, 0, 0, 7, 20, 120, 1, 0, 0, 0},
+	unittype{"Small Office", 0, 0, 0, 100, 20, 0, 4, 40, 1, 0, 0, 0},
+	unittype{"Office Tower", 0, 0, 0, 300, 50, 0, 10, 100, 1, 0, 0, 0},
+	unittype{"Wall", 0, 0, 0, 0, 0, 0, 10, 15, 10, 0, 0, 0},
+	unittype{"Turret", 0, 0, 0, 0, 2, 1, 15, 30, 5, 0, 1, 5},
+	unittype{"Missile Station", 0, 0, 0, 0, 5, 2, 20, 40, 5, 0, 20, 10},
+	unittype{"Cruiser", 0, 0, 0, 0, 10, 4, 40, 90, 5, 5, 5, 7},
+	unittype{"Frigate", 0, 0, 0, 0, 7, 3, 20, 50, 4, 7, 2, 4},
+	unittype{"Destroyer", 0, 0, 0, 0, 10, 4, 35, 90, 5, 10, 2, 5},
+	unittype{"Aircraft Carrier", 0, 0, 0, 0, 50, 6, 50, 120, 15, 5, 10, 10},
 }
 
 // vec represents a 2d vector.
@@ -56,11 +56,13 @@ func (v vec) div(v2 vec) vec {
 
 // unit represents a unit or tile.
 type unit struct {
-	stats    *unitType
+	stats    *unittype
 	health   uint
 	location vec
 	owner    *player
 }
+
+//DOES A MAP OF VECTORS WORK?????
 
 // unitmap is a map of a location on the board to the unit in that location.
 var unitmap = make(map[vec]*unit)
@@ -86,4 +88,24 @@ func (u *unit) damage(d uint) bool { //return value: whether unit was killed
 }
 func (u *unit) attack(target *unit) bool {
 	return target.damage(u.stats.damage)
+}
+
+// newunit creates and initializes a new unit with the given specifications.
+func newunit(stats *unittype, location vec, owner *player) *unit {
+	if unitmap[location] != nil {
+		return nil
+	}
+
+	u := unit{stats, stats.maxhealth, location, owner}
+	unitmap[location] = &u
+	owner.ownedunits = append(owner.ownedunits, &u)
+
+	owner.foodO += stats.foodO
+	owner.metalO += stats.metalO
+	owner.productionO += stats.productionO
+	owner.managementO += stats.managementO
+	owner.peopleR += stats.peopleR
+	owner.managementR += stats.managementR
+
+	return &u
 }
