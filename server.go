d73b8802290e485023c92084f41c6b1a950d8f53
orangebaron <orangebaronroblox@gmail.com>
2017-10-10 20:45:19 -0400

    started making the server

--- server.go
+++ server.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"bytes"
+	"log"
+	"net/http"
 
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func sockethandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	for { // hello world websocket function at the moment
+		_, p, err := conn.ReadMessage()
+		if err != nil {
+			return
+		} else if bytes.Equal(p, []byte("ready")) {
+			break
+		}
+		conn.WriteMessage(websocket.TextMessage, []byte("dummy message"))
+	}
+}
+
+func main() {
+	http.HandleFunc("/", sockethandler)
+	http.Handle("/", http.FileServer(http.Dir("/home/sam/Documents/go/wargame/files")))
+	http.ListenAndServe(":8080", nil)
 }
